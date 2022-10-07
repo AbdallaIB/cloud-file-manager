c0fa20f3d09afd398ead5ac268cd29e86f862eaf
abdallaIb <mrlesway5@gmail.com>
2022-10-07 23:17:02 +0100

    feat(server): add delete + get file functionality

--- server/src/controllers/file.ts
+++ server/src/controllers/file.ts
@@ -3,17 +3,19 @@ const moduleName = '[file] ';
 const logger = loggerHandler(moduleName);
 import db from '@config/database';
 import { Request, Response } from 'express';
-import { uploadMedia } from '@thirdParty/aws';
+import { getSignedUrl, uploadMedia } from '@thirdParty/aws';
 import * as path from 'path';
 import { statusCodes } from '@utils/constants';
 import { filesize } from 'filesize';
 import { ManagedUpload } from 'aws-sdk/clients/s3';
 import { deleteLocalCopy, getMediaType } from '@utils/utils';
+import { RequestWithUser } from 'src/middlewares/auth';
 
 export class FileController {
-  public async saveFile(req: Request, res: Response) {
+  public async saveFile(req: RequestWithUser, res: Response) {
     logger.info('[saveFile][body]', req.body);
     const maxFileSize = 10000000;
+    const { id } = req.user;
     const { file, body } = req;
     const { name } = body;
 
@@ -25,9 +27,7 @@ export class FileController {
     }
 
     const { mimetype, size, filename } = file;
-    const extension = path.extname(filename).toLowerCase();
     const filePath = path.resolve(__dirname, `../../uploads/${filename}`);
-    const mediaType = getMediaType(extension);
 
     if (size > maxFileSize) {
       // If greater than 10MB
@@ -37,11 +37,13 @@ export class FileController {
         message: 'The selected image is too big. Please choose one that is smaller than 10 MB.',
       });
     }
+    const extension = path.extname(filename).toLowerCase();
+    const mediaType = getMediaType(extension);
 
-    const upload = (await uploadMedia(filename, mimetype, filePath, res)) as ManagedUpload.SendData;
-    const query = `INSERT INTO files (name, size, extension, type, url, owner_id) VALUES ($1, $2, $3, $4, $5, $6)`;
-    const params = [name, filesize(size), extension, mediaType, upload.Location, '1'];
     try {
+      const upload = (await uploadMedia(id + '/' + filename, mimetype, filePath, res)) as ManagedUpload.SendData;
+      const query = `INSERT INTO files (name, size, extension, type, url, owner_id) VALUES ($1, $2, $3, $4, $5, $6)`;
+      const params = [name, filesize(size), extension, mediaType, upload.Location, '1'];
       await db.query(query, params);
       res.status(statusCodes.OK).json({ message: 'File uploaded successfully!' });
     } catch (error) {
@@ -51,10 +53,31 @@ export class FileController {
   }
 
   public async getFile(req: Request, res: Response) {
-    logger.info('[getFile][body]', req.body);
+    logger.info('[getFile][params]', req.params);
+    const { id } = req.params;
+    const query = `SELECT * FROM files WHERE id = $1`;
+    const params = [id];
+    try {
+      const result = await db.query(query, params);
+      result.rows[0].url = await getSignedUrl(result.rows[0].url);
+      res.status(statusCodes.OK).json({ message: 'File found!', result: result.rows[0] });
+    } catch (error) {
+      logger.error('[getFile][err]', error);
+      res.status(statusCodes.INTERNAL_SERVER_ERROR).json({ error });
+    }
   }
 
   public async deleteFile(req: Request, res: Response) {
-    logger.info('[deleteFile][body]', req.body);
+    logger.info('[deleteFile][params]', req.params);
+    const { id } = req.params;
+    const query = `DELETE FROM files WHERE id = $1`;
+    const params = [id];
+    try {
+      await db.query(query, params);
+      res.status(statusCodes.OK).json({ message: 'File was deleted!' });
+    } catch (error) {
+      logger.error('[deleteFile][err]', error);
+      res.status(statusCodes.INTERNAL_SERVER_ERROR).json({ error });
+    }
   }
 }
