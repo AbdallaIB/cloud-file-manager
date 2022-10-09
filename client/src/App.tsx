a5f6eacd641f9d85fc77249b70d319ac9604640d
abdallaIb <mrlesway5@gmail.com>
2022-10-09 18:22:57 +0100

    fix(client): remove unused modal code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
 import Header from '@components/header';
-import Modal from '@components/modal';
 import Sidebar from '@components/sidebar';
 import Routes from '@routes/Routes';
 import { useState } from 'react';
@@ -7,26 +6,11 @@ import './App.css';
 
 function App() {
   const [showSidebar, setShowSidebar] = useState(true);
-  const [isModalOpen, setModalState] = useState(true);
 
   return (
     <div className="mainContainer bg-gray-50">
       {/* <div>test</div> */}
       <Sidebar></Sidebar>
-
-      {isModalOpen && (
-        <div>
-          <Modal
-            confirmationData={{
-              isOpen: isModalOpen,
-              onCancel: () => {
-                setModalState(false);
-              },
-              onConfirm: () => {},
-            }}
-          ></Modal>
-        </div>
-      )}
       <div className="flex h-full flex-col w-full">
         <Header></Header>
         <Routes />
