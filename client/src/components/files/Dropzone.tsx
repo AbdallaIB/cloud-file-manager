e3471f5ea7f6521888bde09aee71923d36e3ca32
abdallaIb <mrlesway5@gmail.com>
2022-10-09 18:25:26 +0100

    refact(client): moved dropzone component

--- client/src/components/files/Dropzone.tsx
+++ client/src/components/files/Dropzone.tsx
@@ -1,5 +1,5 @@
+import Button from '@components/shared/Button';
 import { useDropzone } from 'react-dropzone';
-import './index.css';
 interface Props {
   onDrop: (acceptedFiles: File[]) => void;
   open: () => void;
@@ -8,14 +8,18 @@ const Dropzone = ({ onDrop, open }: Props) => {
   const { getRootProps, getInputProps, isDragActive, acceptedFiles } = useDropzone({
     onDrop,
   });
-  const files = acceptedFiles.map((file) => (
-    <li key={file.name}>
-      {file.name} - {file.size} bytes
-    </li>
-  ));
+
   return (
-    <div className="flex justify-center items-center flex-col" style={{ width: '70vw', height: '80vh' }}>
-      <div {...getRootProps({ className: 'dropzone flex justify-center items-center flex-col h-full w-full' })}>
+    <div
+      className="flex justify-center items-center flex-col"
+      style={{ width: '60vw', height: acceptedFiles.length > 0 ? '40vh' : '70vh' }}
+    >
+      <div
+        {...getRootProps({
+          className:
+            'dropzone flex justify-center items-center my-4 flex-col h-full w-full border-dashed border-gray-300 border-2',
+        })}
+      >
         <input className="input-zone" {...getInputProps()} />
         <div className="text-center flex justify-center items-center flex-col h-full w-full gap-4">
           {isDragActive ? (
@@ -24,18 +28,13 @@ const Dropzone = ({ onDrop, open }: Props) => {
             <p className="dropzone-content font-medium text-2xl">Drag & Drop to upload file</p>
           )}
           <p>OR</p>
-          <button
+          <Button text="Browse File" onClick={open} isPrimary={false} color="gray"></Button>
+          {/* <button
             type="button"
-            onClick={open}
             className="flex gap-1 justify-center items-center flex-row shrink-0 rounded bg-gray-200 py-2 px-3 text-gray-600 shadow-sm hover:text-gray-700"
-          >
-            Browse File
-          </button>
+          ></button> */}
         </div>
       </div>
-      <aside>
-        <ul>{files}</ul>
-      </aside>
     </div>
   );
 };
