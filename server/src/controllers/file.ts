41f7cdfab57b43e8394e13664cc7c929724087fa
abdallaIb <mrlesway5@gmail.com>
2022-10-11 15:42:38 +0100

    feat(server): add support for multiple file deletions + delete local file copy after upload + signed media url

--- server/src/controllers/file.ts
+++ server/src/controllers/file.ts
@@ -3,12 +3,12 @@ const moduleName = '[file] ';
 const logger = loggerHandler(moduleName);
 import db from '@config/database';
 import { Request, Response } from 'express';
-import { getSignedUrl, uploadMedia } from '@thirdParty/aws';
+import { deleteMedia, getSignedUrl, uploadMedia } from '@thirdParty/aws';
 import * as path from 'path';
 import { statusCodes } from '@utils/constants';
 import { filesize } from 'filesize';
 import { ManagedUpload } from 'aws-sdk/clients/s3';
-import { getMediaType } from '@utils/utils';
+import { deleteLocalCopy, getMediaType } from '@utils/utils';
 import { RequestWithUser } from '@middlewares/auth';
 
 export class FileController {
@@ -28,18 +28,19 @@ export class FileController {
         const fileSize = filesize(size);
 
         const upload = (await uploadMedia(uId + '/' + filename, mimetype, filePath, res)) as ManagedUpload.SendData;
-        const query = `INSERT INTO files (name, size, extension, type, url, owner_id) VALUES ($1, $2, $3, $4, $5, $6)`;
+        const query = `INSERT INTO files (name, size, extension, type, url, owner_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id`;
         const params = [filename, fileSize, extension, mediaType, upload.Location, uId];
-        await db.query(query, params);
+        const result = await db.query(query, params);
         data.push({
-          id: Math.random(),
+          id: result.rows[0].id,
           name: filename,
-          url: upload.Location,
+          url: getSignedUrl(upload.Location),
           created_at: new Date().toISOString(),
           size: fileSize,
           type: mediaType,
         });
         console.log('uploading...');
+        deleteLocalCopy(filePath);
       }
       console.log('done');
       res.status(statusCodes.OK).json({ message: 'Files uploaded successfully!', data });
@@ -56,11 +57,16 @@ export class FileController {
     const params = [uId];
     try {
       const result = await db.query(query, params);
-      console.log(result.rows);
-      result.rows[0].url = await getSignedUrl(result.rows[0].url);
-      res.status(statusCodes.OK).json({ message: 'File found!', data: result.rows });
+      let files = [];
+      if (result && result.rows && result.rows.length > 0) {
+        files = result.rows;
+        for (const file of files) {
+          if (file) file.url = getSignedUrl(file.url);
+        }
+      }
+      res.status(statusCodes.OK).json({ message: 'File found!', data: files });
     } catch (error) {
-      logger.error('[getUserFiles][err]', error);
+      logger.error('[getUserFiles][err]', error.message);
       res.status(statusCodes.INTERNAL_SERVER_ERROR).json({ error });
     }
   }
@@ -72,7 +78,7 @@ export class FileController {
     const params = [id];
     try {
       const result = await db.query(query, params);
-      result.rows[0].url = await getSignedUrl(result.rows[0].url);
+      result.rows[0].url = getSignedUrl(result.rows[0].url);
       res.status(statusCodes.OK).json({ message: 'File found!', data: result.rows[0] });
     } catch (error) {
       logger.error('[getFile][err]', error);
@@ -80,14 +86,19 @@ export class FileController {
     }
   }
 
-  public async deleteFile(req: Request, res: Response) {
-    logger.info('[deleteFile][params]', req.params);
-    const { id } = req.params;
-    const query = `DELETE FROM files WHERE id = $1`;
-    const params = [id];
+  public async deleteFile(req: RequestWithUser, res: Response) {
+    logger.info('[deleteFile][body]', req.body);
+    const { uId } = req.user;
+    const { files } = req.body as unknown as { files: { id: number; name: string }[] };
     try {
-      await db.query(query, params);
-      res.status(statusCodes.OK).json({ message: 'File was deleted!' });
+      for (const file of files) {
+        const query = `DELETE FROM files WHERE id = $1`;
+        const params = [file.id];
+        await db.query(query, params);
+        // delete file from s3 bucket
+        await deleteMedia(uId + '/' + file.name, res);
+      }
+      res.status(statusCodes.OK).json({ message: 'Files were deleted!' });
     } catch (error) {
       logger.error('[deleteFile][err]', error);
       res.status(statusCodes.INTERNAL_SERVER_ERROR).json({ error });
